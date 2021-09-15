b60c5bc879a475361bf59db9e53b61d855492746
Ilya Panarin <panarinila82@gmail.com>
2021-09-15 13:03:32 +0300

    initial commit

---
+++ color_picker/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <div>
+    <h2>Color</h2>,
+    <div className="colorCard">#58D68D</div>,
+    document.querySelector("#root")
+  </div>
+);
